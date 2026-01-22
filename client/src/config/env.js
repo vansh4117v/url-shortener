14bdb75842d94c6d33434849a0c6c4e67c536be5
Vansh Gupta <vanshgupta9917@gmail.com>
2026-01-23 00:36:28 +0530

    update API_BASE_URL default value in env configuration

--- client/src/config/env.js
+++ client/src/config/env.js
@@ -1,5 +1,5 @@
   export const env = {
-  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || 'http://localhost:5000/api',
+  API_BASE_URL: import.meta.env.VITE_API_BASE_URL || '/api',
   SITE_NAME: import.meta.env.VITE_SITE_NAME,
   IS_DEV: import.meta.env.DEV,
   IS_PROD: import.meta.env.PROD,
